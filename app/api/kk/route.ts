5a4cbd546cbda5c81503da6c85ff4aaa27e914e0
zyzik1248 <zuzannadudzinska96@gmail.com>
2026-09-24 20:23:08 +0200

    fix

--- app/api/kk/route.ts
+++ app/api/kk/route.ts
@@ -1,20 +1,25 @@
 import { getStore } from "@netlify/blobs";
 import { NextResponse } from "next/server";
 
-const store = getStore("products");
-
 export async function GET() {
-  const products = await store.get("products", { type: "json" });
+  const store = getStore("products");
+
+  const products = await store.get("products", {
+    type: "json",
+  });
 
   return NextResponse.json(products ?? []);
 }
 
 export async function POST(request: Request) {
+  const store = getStore("products");
+
   const product = await request.json();
 
-  const products = (await store.get("products", {
-    type: "json",
-  })) ?? [];
+  const products =
+    (await store.get("products", {
+      type: "json",
+    })) ?? [];
 
   const newProduct = {
     id: crypto.randomUUID(),
